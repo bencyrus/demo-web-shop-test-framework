1f5d9031686d80f62c787d45a87da3b28ef0b2d3
BenCodesForSchool <bjweiler92@gmail.com>
2023-05-15 13:36:04 -0400

    Added comments to nav_bar.cy.ts and gave it a directory

--- cypress/e2e/tests-without-login/nav_bar.cy.ts
+++ cypress/e2e/tests-without-login/nav_bar.cy.ts
@@ -1,8 +1,22 @@
+//This test case ensures that all navigation bar (otherwise known as the top menu, or the header menu) items can be clicked and navigate to the pages
+//they are expected to navigate to. 
+//The test case will fail if any of the navigation bar items do not lead to their expected pages
+/*
+Step Number	Description	                        Object	                    Data	        Expected Result
+1	        Navigate to Demo Web Shop			                                            Main page of Demo Web Shop is displayed
+2	        Click on "Books" button	            Books button		                        Books category page is displayed
+3	        Click on "Computers" button	        Computers button		                    Computers category page is displayed
+4	        Click on "Electronics" button	    Electronics button		                    Electronics category page is displayed
+5	        Click on "Apparel & Shoes" button	Apparel & Shoes button		                Apparel & Shoes category page is displayed
+6	        Click on "Digital Downloads" button	Digital Downloads button                    Digital Downloads category page is displayed
+7	        Click on "Jewelry" button	        Jewelry button		                        Jewelry category page is displayed
+8	        Click on "Gift Cards" button	    Gift Cards button		                    Gift Cards category page is displayed
+*/
+
 describe('Navigation bar testing', () => {
 
     it('should navigate to the website homepage', () => {
         cy.loadWebsite()
-        cy.log('Website loaded')
     })
 
     it('should click "BOOKS" on the navigation bar and verify that clicking the link navigates to the books category page', () => {
